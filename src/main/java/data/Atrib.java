f76baf09f7d728a7a0ed579b67f6aa0a92e07f91
Roskar333 <oskar23001@student.hamk.fi>
2024-04-30 21:05:06 +0300

    Added basics of database
    Needs lots of work

---
+++ src/main/java/data/Atrib.java
@@ -0,0 +1,7 @@
+package data;
+
+public class Atrib {
+	//Shares attributes to the service from the robot
+	//What attributes do we want?
+	//Place getters and setters for speed, turnangle, times looped, avoidance distance or whatever here
+}
--- src/main/java/services/RobotServices.java
+++ src/main/java/services/RobotServices.java
@@ -1,5 +1,24 @@
 package services;
 
-public class RobotServices {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import conn.Connections;
+import data.Atrib;
+
+@Path("/robot")
+public class RobotServices {
+	// Main service class, which inserts data into a database based on input
+	// Attributes class has the needed values
+	// Has the method by which robot reads the database
 }
